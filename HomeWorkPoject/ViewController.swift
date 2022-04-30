09f754d543ac31d35878f9cfa8b79c6289c2769e
Gerbilin <gerbilin@yandex.ru>
2022-04-30 13:33:35 +0300

    Initial Commit

---
+++ HomeWorkPoject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWorkPoject
+//
+//  Created by Gerbilin Mac on 30.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
